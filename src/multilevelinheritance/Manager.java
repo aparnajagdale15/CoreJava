9a5bc60b9b0216806b24d23e24a3f31c1cd771f5
aparnajagdale15 <aparnajagdale15@gmail.com>
2026-08-03 15:33:54 +0530

    Message

---
+++ src/multilevelinheritance/Manager.java
@@ -0,0 +1,8 @@
+package multilevelinheritance;
+
+public class Manager extends Employee {
+	void manager() {
+		System.out.println("This is a manager. ");
+	}
+
+}
---
+++ src/singleinheritance/Animal.java
@@ -0,0 +1,9 @@
+package singleinheritance;
+
+public class Animal {
+	void eat() {
+		System.out.println("Eating");
+	}
+	
+
+}
